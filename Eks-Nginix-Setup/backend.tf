99d840496cd19194fddbb518a8723042b839be05
gsaikrishna515 <79789355+gsaikrishna515@users.noreply.github.com>
2025-09-22 20:58:08 +0530

    Update backend.tf

--- Eks-Nginix-Setup/backend.tf
+++ Eks-Nginix-Setup/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     # Use the name of the S3 bucket you just created
     bucket         = "cicd-tf-eks-sai" # <-- USE THE SAME NAME AS ABOVE
-    key            = "jenkins/terraform.tfstate"         # The path/name of the state file within the bucket
+    key            = "jenkins-k8s/terraform.tfstate"         # The path/name of the state file within the bucket
     region         = "ap-south-1"
 
     # Use the name of the DynamoDB table you just created
